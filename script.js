2d1c0da420562cef5432a5710e013bdfd60f3272
Nvlahogiannis12 <526Nvlahogiannis@frhsd.com>
2025-07-29 17:34:15 +0000

    cards

---
+++ script.js
@@ -0,0 +1,62 @@
+const websiteCards = [
+{
+Photo:"",
+Name:"F. Scott Fitzgerald",
+About:"The Life and Accomplishments of writer F. Scott Fitzgerald creator of 'The Great Gatsby'.",
+Date:"October 2024",
+},
+{
+Photo:"",
+Name:"Mad-Libs",
+About:"Create your story by entering in the Nouns, Verbs, & Adjectives of your choosing in this exciting randomly generated story website",
+Date:"November 2024",
+},
+{
+Photo:"",
+Name:"Name Generator",
+About:"Generate your fantasy name and title to rule with.",
+Date:"December 2024",
+},
+{
+Photo:"",
+Name:"Rock, Paper, Scissors",
+About:"Fight your way through games of Rock, Paper, & Scissors. Play the well known Classic or take on other modes",
+Date:"January 2025",
+},
+{
+Photo:"",
+Name:"President Website",
+About:"This Website is a biography of the former President: James Madison",
+Date:"February 2025",
+},
+{
+Photo:"",
+Name:"Hangman Smash",
+About:"Super Smash Bros is the topic of this version of the beloved and classic game Hangman",
+Date:"March 2025",
+},
+{
+Photo:"",
+Name:"Java Knight",
+About:"In this RPG take on randomly generated enemies in multiple different environments with turn based combat",
+Date:"April 2025",
+},
+{
+Photo:"",
+Name:"Majora's Mask Cards",
+About:"Get the know the townsfolk of Termina from the N64 game, The Legend of Zelda: Majora's Mask",
+Date:"May 2025",
+},
+{
+Photo:"",
+Name:"Toy Bird Co.",
+About:"Browse the shop of the handcrafted kid's toy shop",
+Date:"June 2025",
+},
+{
+Photo:"",
+Name:"Chopsticks",
+About:"Play the game chopsticks against a computer",
+Date:"July 2025",
+},
+]
