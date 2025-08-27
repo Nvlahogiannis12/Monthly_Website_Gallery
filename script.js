461f34600642cca8bad58857655e70895a3ed275
Nvlahogiannis12 <526Nvlahogiannis@frhsd.com>
2025-08-27 14:08:40 +0000

    photos added to cards

--- script.js
+++ script.js
@@ -1,69 +1,69 @@
 const websiteCards = [
 {
-Photo:"",
+Photo:"imgs/Gatsby_Website.png",
 Name:"F. Scott Fitzgerald",
 About:"The Life and Accomplishments of writer F. Scott Fitzgerald creator of 'The Great Gatsby'.",
 Date:"October 2024",
 Website:"https://nvlahogiannis12.github.io/Look_Ma_Im_Famous/",
 },
 {
-Photo:"",
+Photo:"imgs/MadLib_Website.png",
 Name:"Mad-Libs",
 About:"Create your story by entering in the Nouns, Verbs, & Adjectives of your choosing in this exciting randomly generated story website",
 Date:"November 2024",
 Website:"https://nvlahogiannis12.github.io/Mad-LIB/",
 },
 {
-Photo:"",
+Photo:"imgs/Name_Gen_Website.png",
 Name:"Name Generator",
-About:"Generate your fantasy name and title to rule with.",
+About:"Generate your fantasy title to rule with",
 Date:"December 2024",
 Website:"https://nvlahogiannis12.github.io/Name-Gen-Dec/",
 },
 {
-Photo:"",
+Photo:"imgs/RPS_Website.png",
 Name:"Rock, Paper, Scissors",
 About:"Fight your way through games of Rock, Paper, & Scissors. Play the well known Classic or take on other modes",
 Date:"January 2025",
 Website:"https://nvlahogiannis12.github.io/Rock-Paper-Scissors/",
 },
 {
-Photo:"",
+Photo:"imgs/Presidential_Website.png",
 Name:"President Website",
 About:"This Website is a biography of the former President: James Madison",
 Date:"February 2025",
 Website:"https://nvlahogiannis12.github.io/James-Madison-Feb/",
 },
 {
-Photo:"",
+Photo:"imgs/SuperHangmanSmash_Website.png",
 Name:"Hangman Smash",
 About:"Super Smash Bros is the topic of this version of the beloved and classic game Hangman",
 Date:"March 2025",
 Website:"https://nvlahogiannis12.github.io/Sham-Hang-Mar/",
 },
 {
-Photo:"",
+Photo:"imgs/Java_Knight_Website.png",
 Name:"Java Knight",
 About:"In this RPG take on randomly generated enemies in multiple different environments with turn based combat",
 Date:"April 2025",
 Website:"https://nvlahogiannis12.github.io/JavaKnight/",
 },
 {
-Photo:"",
+Photo:"imgs/Majora's_Mask_Cards_Website.png",
 Name:"Majora's Mask Cards",
 About:"Get the know the townsfolk of Termina from the N64 game, The Legend of Zelda: Majora's Mask",
 Date:"May 2025",
 Website:"https://nvlahogiannis12.github.io/May-Majora-s_Mask/",
 },
 {
-Photo:"",
+Photo:"imgs/ToyBird_Website.png",
 Name:"Toy Bird Co.",
 About:"Browse the shop of the handcrafted kid's toy shop",
 Date:"June 2025",
 Website:"https://nvlahogiannis12.github.io/Toy-Birb-Co/",
 },
 {
-Photo:"",
+Photo:"imgs/ChopSticks_Website.png",
 Name:"Chopsticks",
 About:"Play the game chopsticks against a computer",
 Date:"July 2025",
@@ -97,7 +97,7 @@ Website:"https://nvlahogiannis12.github.io/ChopSticks/",
                     </p>
             <a href="${websiteCards.Website}">
             <button class="btn btn-sm btn-primary show-info-btn" data-player-index="${index}" data-bs-toggle="modal" data-bs-target="#playerModal">
-            More Info
+            Visit
           </button>
             </a>
 
